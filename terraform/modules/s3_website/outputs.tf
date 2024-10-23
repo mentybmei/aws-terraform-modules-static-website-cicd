bfd92c749002f2b645f830d7af475b22d804fe58
Annie-Mei Forster <annie-meiforster@Annie-Meis-MacBook-Pro-2.local>
2024-10-23 19:48:04 +1100

    Implement S3_CloudFront

--- terraform/modules/s3_website/outputs.tf
+++ terraform/modules/s3_website/outputs.tf
@@ -18,7 +18,7 @@ output "index_document" {
     value = var.index_document
 }
 
-output "bucket_region_domain_name" {
+output "bucket_regional_domain_name" {
     description = "The regional domain name of the S3 bucket"
     value = aws_s3_bucket.website_bucket.bucket_regional_domain_name
 }
