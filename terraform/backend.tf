87f6b3c697e91da0487f258075d125216eaa6986
mentybmei <anniemei.forster@gmail.com>
2025-10-26 16:45:51 +1100

    Change S3 bucket name for Terraform backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket          = "aminthecloud.com"
+        bucket          = "terraform-state-bucket-muntjakmei"
         key             = "website/terraform.tfstate"
         region          = "us-east-1"
         dynamodb_table  = "terraform_state_locks"
