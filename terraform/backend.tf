4d7542955f02eceb294067c0cc36e97114a6c0a8
mentybmei <anniemei.forster@gmail.com>
2025-10-26 16:41:46 +1100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket          = "terraform-state-bucket-muntjakmei"
+        bucket          = "aminthecloud.com"
         key             = "website/terraform.tfstate"
         region          = "us-east-1"
         dynamodb_table  = "terraform_state_locks"
